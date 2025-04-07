d0140996e4a6fa59d42ce1d7fc5581c1a82c334c
KIE_Admin <z.szulakowska.985@studms.ug.edu.pl>
2025-04-07 15:36:57 +0200

    functions

---
+++ Zadanie 2 funkcje/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2_funkcje
+{//Napisz funkcję o nazwie max, która wyznacza maksimum z dwóch liczb rzeczywistych
+    //zadanych jako argumenty tej funkcji.Napisz program wykorzystujący funkcję max.
+
+    internal class Program
+    { static double max(double x, double y)
+            {if (x>y) return x;
+            else return y;
+            }
+
+        static void Main(string[] args)
+        {
+        Console.WriteLine("max z 3 i 8 to {0}", max (3,8));
+        Console.WriteLine("max z 3 i 2 to {0}", max(3, 2));
+        Console.WriteLine("max z 5 i 12 to {0}", max(5, 12));
+        Console.ReadKey();
+    }
+    }
+}
---
+++ Zadanie 3 funkcje/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3_funkcje
+{//Napisz funkcję o nazwie pole_kola, która posiada jeden parametr r-promień koła i zwraca
+    //obliczoną wartość pola koła.Napisz program wykorzystujący funkcję pole_kola.
+
+    internal class Program
+    { 
+     static double pole_kola(double r)
+    {
+        return Math.PI* r *r;
+            }
+        static void Main(string[] args)
+        {
+        Console.WriteLine("dla r= 5.5 pole wynosi {0}", pole_kola(5.5));
+        Console.ReadKey();
+        }
+    }
+}
