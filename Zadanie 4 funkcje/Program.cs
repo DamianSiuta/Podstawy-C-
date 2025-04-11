4876d74c45ef20147896192a198070532cffcb72
Damian Siuta <dami.siuta@gmail.com>
2025-04-11 12:56:52 +0200

    :]

---
+++ Zadanie 4 funkcje/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Zadanie_4_funkcje
+{
+    internal class Program
+    {
+        static int ile_cyfr(int liczba)
+        {
+            
+            if (liczba == 0)
+            {
+                return 1;
+            }
+
+            int i = 0;
+            while (liczba != 0)
+            {
+                liczba /= 10;
+                i++;
+            }
+            return i;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Liczba zero ma {0} cyfr", ile_cyfr(0));
+            Console.WriteLine("Liczba 12345 ma {0} cyfr", ile_cyfr(12345));
+            Console.WriteLine("Liczba -9876 ma {0} cyfr", ile_cyfr(-9876));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zadanie 5 funkcje/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_5_funkcje
+{//Napisz funkcję, która sprawdza czy jej argument jest liczbą pierwszą. Jako wartość funkcja
+ // powinna zwracać 1 dla liczb pierwszych i 0 dla pozostałych liczb.Przetestuj działanie
+ //             utworzonej funkcji.
+
+    internal class Program
+        
+    {
+        static int liczba_pierwsza(int liczba)
+        {
+            int i;
+            for (i = 2; i < liczba; i++)
+                if (liczba % i == 0)
+                    return 0;
+            return 1;
+
+               
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"czy 12 jest liczba 1. {liczba_pierwsza(12)}");
+            Console.WriteLine($"czy 13 jest liczba 1. {liczba_pierwsza(13)}");
+            Console.ReadKey();
+
+        }
+    }
+}
